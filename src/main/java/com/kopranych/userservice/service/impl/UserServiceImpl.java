7b7e49f5b75d95bcb828f4f951b9074a1cdc1388
Kopranych <kopranych@gmail.com>
2018-08-31 20:06:34 +0300

    [UPDATE] - UserServiceImpl реализовал методы для работы с сервисом

--- src/main/java/com/kopranych/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/kopranych/userservice/service/impl/UserServiceImpl.java
@@ -1,23 +1,28 @@
 package com.kopranych.userservice.service.impl;
 
 import com.kopranych.userservice.model.User;
+import com.kopranych.userservice.repository.UserRepository;
 import com.kopranych.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User addNewUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public void deleteUser(Long userId) {
-
+        userRepository.deleteById(userId);
     }
 
     @Override
     public User getUserByEmail(String email) {
-        return null;
+        return userRepository.findByUserEmail(email);
     }
 }
