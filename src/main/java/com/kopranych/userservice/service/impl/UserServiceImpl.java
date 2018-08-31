c74e6f9753c59a299c74b1c62aef69a4b7aa6e14
Kopranych <kopranych@gmail.com>
2018-08-31 23:09:07 +0300

    [ADD] - UserServiceImpl добавил хеширование пароля пользователя

--- src/main/java/com/kopranych/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/kopranych/userservice/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.kopranych.userservice.service.impl;
 
 import com.kopranych.userservice.model.User;
 import com.kopranych.userservice.repository.UserRepository;
+import com.kopranych.userservice.service.Encryptor;
 import com.kopranych.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private Encryptor encryptor;
     @Override
     public User addNewUser(User user) {
+        String hashPassword = encryptor.securePassword(user.getHashPassword());
+        user.setHashPassword(hashPassword);
         return userRepository.save(user);
     }
 
