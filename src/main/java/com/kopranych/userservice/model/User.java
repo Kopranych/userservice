c52b313cb9beb6c0039a4e49e292a4bda912404d
Kopranych <kopranych@gmail.com>
2018-08-31 20:03:02 +0300

    [ADD] - User добавил конструктор с использованием полей кроме id

--- src/main/java/com/kopranych/userservice/model/User.java
+++ src/main/java/com/kopranych/userservice/model/User.java
@@ -1,24 +1,36 @@
 package com.kopranych.userservice.model;
 
 
-import lombok.Data;
-import org.springframework.data.annotation.Id;
+import lombok.*;
 
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @Entity
+@Table(name = "users")
 public class User {
     @Id
+    @Column(name = "ID")
     @GeneratedValue
-    private int id;
+    private Long id;
+    @Column(name = "FIRSTNAME")
     private String firstName;
-    private String lastname;
+    @Column(name = "LASTNAME")
+    private String lastName;
+    @Column(name = "BIRTHDATE")
     private LocalDate birthDate;
+    @Column(name = "EMAIL")
     private String userEmail;
+    @Column(name = "PASSWORD")
     private String hashPassword;
 
+    public User(String firstName, String lastName, LocalDate birthDate, String userEmail, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.userEmail = userEmail;
+        //TODO добавить функцию хэширования пароля
+        this.hashPassword = password;
+    }
 }
